c1007ac5ea4904fd60517f0b8f6c2949a3c9acee
xjane <tinklexjane@gmail.com>
2017-08-14 21:59:51 +0800

    format datetime & select one

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def display_event_status(event)
+    case event.status
+      when "draft"
+        "草稿"
+      when "public"
+        "公开"
+      when "private"
+        "私密"
+      else
+        ""
+    end
+  end
 end
