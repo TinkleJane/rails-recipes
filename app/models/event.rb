aeba36c786c8420b5c4bb24b9f2e5bd6a45ca4c5
xjane <tinklexjane@gmail.com>
2017-08-15 23:05:03 +0800

    select2 plugin for one

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,8 @@ class Event < ApplicationRecord
  before_validation :generate_friendly_id, :on => :create
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+ belongs_to :category, :optional => true
+
  def to_param
    self.friendly_id
  end
